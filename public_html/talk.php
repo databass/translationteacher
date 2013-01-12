1b04340b3d4299a71c7ed344c8e160488e79c755
David Byman <dbvt@databass.com>
2013-01-11 19:16:40 -0500

    Update public_html/talk.php
    simplified parameters...just have 'phrase' instead of 'tphrase' and 'bphrase'

--- public_html/talk.php
+++ public_html/talk.php
@@ -1,13 +1,23 @@
 <?php
 
-require "../lib/WritePhrase.php";
-$bPhrase=$_GET['bPhrase'];
-$tPhrase=$_GET['tPhrase'];
-$lang=$_GET['lang'];
-$format=$_GET['format'];
+/*
+ *  Converts text into an mp3 or wav file, in the supplied language. 
+ *
+ *  @param $phrase string The text to be written
+ *  @param $lang string Two-character language code. Defaults to 'en' (English)
+ *  @param $format String Audio format. Currently supported values are 'wav' and 'mp3'. Defaults to 'mp3' Currently only'mp3' will work with native voices in non-English languages 
+ *
+ *  Author: David Byman david.byman@gmail.com
+ */
 
-$writePhrase = new WritePhrase($bPhrase, $tPhrase, $lang, $format);
+
+require "../lib/WritePhrase.php";
+
+$phrase = $_GET['phrase'];
+$lang   = $_GET['lang'];
+$format = $_GET['format'];
+
+$writePhrase = new WritePhrase($phrase, $lang, $format);
 
 
 ?>
-
